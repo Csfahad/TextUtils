57732d0a105c206f171ab362f7ca07c7c5fad1eb
Csfahad <76874725+Csfahad@users.noreply.github.com>
2021-12-07 20:40:35 +0530

    Final Bug Fix

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -25,10 +25,7 @@ export default function TextForm(props) {
     }
 
     const handleCopy = ()=> {
-        var text = document.getElementById("myBox");
-        text.select();
-        navigator.clipboard.writeText(text.value);
-        document.getSelection().removeAllRanges();
+        navigator.clipboard.writeText(text);
         props.showAlert("Copied to clipboard!", "success");
 
     }
@@ -57,8 +54,8 @@ export default function TextForm(props) {
 
         <div className="container my-3" style={{backgroundColor: props.mode===`dark`?`#181a1c` : `white`, color: props.mode===`dark`?`white` : `#181a1c`}}>
             <h3>Your text Summary :</h3>
-            <p>{text.split(" ").filter((element)=> {return element.length!==0}).length} words and {text.length} characters</p>
-            <p>{0.008 * text.split(" ").length} Minutes read</p>
+            <p>{text.split(/\s+/).filter((element)=> {return element.length!==0}).length} words and {text.length} characters</p>
+            <p>{0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length} Minutes read</p>
             <h3>Preview</h3>
             <p>{text.length>0 ? text : "Nothing to preview..."}</p>
         </div>
